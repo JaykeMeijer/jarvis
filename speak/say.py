ee3574c78af151891a7b8431df6cf196e79eeaa1
Jayke Meijer <jayke@xomnia.com>
2021-02-22 11:29:05 +0100

    Dockerized speak

--- speak/say.py
+++ speak/say.py
@@ -1,19 +1,30 @@
 import redis
 from time import sleep
 import espeakng
+import os
 
+
+print("Initializing speech engine")
 
 speaker = espeakng.Speaker()
 speaker.voice = 'mb-en1'
 speaker.wpm = 150
 speaker.pitch = 75
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+print("Setting up REDIS connection")
+
+r = redis.Redis(
+    host=os.environ.get('REDIS_HOST'),
+    port=os.environ.get('REDIS_PORT'),
+    db=0
+)
 
 ps = r.pubsub()
 
-ps.subscribe('say')
+ps.subscribe(os.environ.get('REDIS_TOPIC'))
 
+
+print("Ready to speak")
 
 while True:
     msg = ps.get_message()
